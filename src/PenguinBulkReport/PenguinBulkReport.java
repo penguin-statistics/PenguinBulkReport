558fe3755f9145eaaf970b991be65ac9db0c8d96
Zhuosi He <zhuosi.he@mail.utoronto.ca>
2019-08-08 03:13:30 -0400

    add back the empty upload

--- src/PenguinBulkReport/PenguinBulkReport.java
+++ src/PenguinBulkReport/PenguinBulkReport.java
@@ -314,14 +314,14 @@ public class PenguinBulkReport {
                 params.put("source","penguin bulk report");//either 1 | 0
                 System.out.println(params.toString());
                 infos.add(params.toString());
-                int report_status = report(stageID,summary_drop,furniture_num,userID);
-                if (report_status>201){
-                    System.out.println("Error! Error code is:");
-                    System.out.println(report_status);
-                } else{
-                    System.out.println(report_status);
-                    System.out.println("Upload Successful");
-                }
+//                int report_status = report(stageID,summary_drop,furniture_num,userID);
+//                if (report_status>201){
+//                    System.out.println("Error! Error code is:");
+//                    System.out.println(report_status);
+//                } else{
+//                    System.out.println(report_status);
+//                    System.out.println("Upload Successful");
+//                }
 
             }// for ends
 
